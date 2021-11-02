b51ad3ecd3174a8dcdff54936d4f7feb429b6718
Emma HB <emmanuelle.bolognini@gmail.com>
2021-11-02 21:27:56 +0100

    Iteration 5

--- app.js
+++ app.js
@@ -7,14 +7,13 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+// Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Register the location for handlebars partials here:
-
-// ...
 
 // Add the route handlers here:
 
