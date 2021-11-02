d4aae30da836222e734492312a302a5883e705eb
Auriane Viggiano <auriane.vigg@gmail.com>
2021-11-02 19:12:41 +0100

    iteration 1+css

--- app.js
+++ app.js
@@ -8,23 +8,23 @@ const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-app.get('/', (request, response, next) => response.sendFile(__dirname + '/index'));
-app.get('/beers', (request, response, next) => response.sendFile(__dirname + '/'));
-app.get('/random-beer', (request, response, next) => response.sendFile(__dirname + '/'));
+//app.get('/', (request, response, next) => );
+//app.get('/beers', (request, response, next) => response.sendFile(__dirname + '/'));
+//app.get('/random-beer', (request, response, next) => response.sendFile(__dirname + '/'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-    // const data = 
+    const data = {};
     res.render('index', data);
 });
 
-app.get()
+//app.get()
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
